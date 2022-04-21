26c6deeb0317e7fc83c208f536d3722f4b38c15f
Sofia <sofia.montgomery@gmail.com>
2022-04-21 14:46:04 +0200

    added form

--- src/Pages/EventForm.jsx
+++ src/Pages/EventForm.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Form from './../components/Form/Form'
 
 function EventForm(){
     return(
-        <div>Create event here</div>
+        <Form/>
     )}
 export default EventForm;
