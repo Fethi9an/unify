2e6021db35aea0c2f26874c831a061694b71ede0
Sofia <sofia.montgomery@gmail.com>
2022-04-21 14:26:57 +0200

    edit app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './normalize.css'
 import './App.css'
+import Topnavbar from './containers/Topnavbar'
+import Footer from './components/Footer/footer'
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './Pages/Home'
+import EventForm from './Pages/EventForm'
+import Profile from './Pages/Profile'
 
 function App() {
   return (
+      <Router>
+        <div id="page-container">
+          <Topnavbar/>
+          <hr></hr>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/eventform" element={<EventForm/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          
 
+        </Routes>
+        <Footer/>
+      </Router>
   )
 }
 export default App
-
-
