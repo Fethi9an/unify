632a4c8e36f8fdd988008887c9380006f996da6b
Juliosc1 <julio.siklander@gmail.com>
2022-04-21 13:58:42 +0200

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
 import './normalize.css'
 import './App.css'
-import Topnavbar from './containers/Topnavbar'
-import Footer from './components/Footer/footer'
-import Form from './components/Form/Form'
 
 function App() {
   
   return (
   
       <div id="page-container">
-        <Topnavbar/>
-        <hr></hr>
-        <p id='blickf'>I Blickfånget</p>
-        <Form />
-        <Footer />
+        
       </div>
   )
   
